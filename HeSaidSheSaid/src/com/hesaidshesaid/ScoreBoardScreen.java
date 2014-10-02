b11af00968e4b116164e18b18252216dbe939e1e
NoahPena <noahpena7@utexas.edu>
2014-10-02 14:38:04 -0500

    really didn't accomplish much

--- HeSaidSheSaid/src/com/hesaidshesaid/ScoreBoardScreen.java
+++ HeSaidSheSaid/src/com/hesaidshesaid/ScoreBoardScreen.java
@@ -1,18 +1,37 @@
 package com.hesaidshesaid;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class ScoreBoardScreen extends Activity implements OnClickListener {
+	
+	Button buttonOKScoreBoard;
+	
+	ListView listViewScoreBoard;
 	
 	public void onCreate(Bundle bundleInstance)
 	{
 		super.onCreate(bundleInstance);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.score_board_screen);
+		
+		Collections.sort(GlobalVariables.players, new Comparator<Player>() {
+			@Override public int compare(Player p1, Player p2) {
+	            return p1.getPlayerScore() - p2.getPlayerScore(); // Ascending
+			}
+		});
+	
 	}
 
 	@Override
@@ -20,5 +39,5 @@ public class ScoreBoardScreen extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 }
